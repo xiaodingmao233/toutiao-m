319aff09843a51d69e8d7833163fc0c48c2d8267
lizheng <2112276441@qq.com>
2021-03-05 22:30:11 +0800

    项目初始化--rem适配

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载 Vant 组件库
+import Vant from 'vant'
+// 加载 Vant 组件库样式
+import 'vant/lib/index.css'
+
+// 自动设置 rem 基准值 (html标签字体大小)
+import 'amfe-flexible'
+
+// 全局注册 Vant 中的组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
