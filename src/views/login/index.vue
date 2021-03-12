c3e6a9d3384e91fd83ef6674b057a1cbf115c397
lizheng <2112276441@qq.com>
2021-03-12 22:00:24 +0800

    登录页面--页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="login-container">
-    登录页面
+    <van-nav-bar
+      title="登录"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- 登录表单 -->
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        left-icon="smile-o"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        left-icon="music-o"
+        placeholder="请输入验证码"
+      >
+        <template #button>
+          <van-button
+            size="small"
+            round
+          >发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <!-- /登录表单 -->
+    <van-button type="info" block>信息按钮</van-button>
   </div>
 </template>
 
@@ -11,14 +38,18 @@ export default {
   props: {},
   data () {
     return {
-
+      user: {
+        mobile: '',
+        code: ''
+      }
     }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+  }
 }
 </script>
 
